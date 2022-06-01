3a23b7270d57cc7cc49d73838110dcf017d6a062
serroukh <serroukh94@gmail.com>
2022-06-01 13:52:23 +0200

    Modif Test Task

--- tests/Entity/TaskTest.php
+++ tests/Entity/TaskTest.php
@@ -8,44 +8,44 @@ use PHPUnit\Framework\TestCase;
 
 class TaskTest extends TestCase
 {
-    public function testGetId()
+    public function testGetId(): void
     {
         $task = new Task();
-        static::assertEquals($task->getId(), null);
+        static::assertEquals(null, $task->getId());
     }
 
-    public function testGetSetTitle()
+    public function testGetSetTitle(): void
     {
         $task = new Task();
         $task->setTitle('Test title');
-        static::assertEquals($task->getTitle(), 'Test title');
+        static::assertEquals('Test title', $task->getTitle());
     }
 
-    public function testGetSetContent()
+    public function testGetSetContent(): void
     {
         $task = new Task();
         $task->setContent('Test content');
-        static::assertEquals($task->getContent(), 'Test content');
+        static::assertEquals('Test content', $task->getContent());
     }
 
-    public function testGetSetCreatedAt()
+    public function testGetSetCreatedAt(): void
     {
         $task = new Task();
         $task->setCreatedAt(new \DateTime);
         static::assertInstanceOf(\DateTime::class, $task->getCreatedAt());
     }
 
-    public function testGetSetAuthor()
+    public function testGetSetAuthor(): void
     {
         $task = new Task();
         $task->setAuthor(new User);
         static::assertInstanceOf(User::class, $task->getAuthor());
     }
 
-    public function testIsDoneToggle()
+    public function testIsDoneToggle(): void
     {
         $task = new Task();
         $task->toggle(true);
-        static::assertEquals($task->isDone(), true);
+        static::assertEquals(true, $task->isDone());
     }
 }
