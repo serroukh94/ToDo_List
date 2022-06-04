4a9c89b0f24015f347b78e77e64f6784c644b3c5
serroukh <serroukh94@gmail.com>
2022-06-04 05:06:15 +0200

    Test UserController

---
+++ tests/Controller/UserControllerTest.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use App\Tests\DataFixtures\DataFixtureTestCase;
+
+
+class UserControllerTest extends DataFixtureTestCase
+{
+    public function testListActionWithoutLogin(): void
+    {
+        // If the user isn't logged, should redirect to the login page
+        self::ensureKernelShutdown();
+        $client = static::createClient();
+        $client->request('GET', '/users');
+        static::assertSame(302, $client->getResponse()->getStatusCode());
+
+        $crawler = $client->followRedirect();
+        // Test if login field exists
+        static::assertSame(1, $crawler->filter('input[name="_username"]')->count());
+        static::assertSame(1, $crawler->filter('input[name="_password"]')->count());
+    }
+
+    public function testListAction(): void
+    {
+        $securityControllerTest = new SecurityControllerTest();
+        $client = $securityControllerTest->testLoginAsAdmin();
+
+        $crawler = $client->request('GET', '/users');
+        static::assertSame(200, $client->getResponse()->getStatusCode());
+        static::assertSame("Liste des utilisateurs", $crawler->filter('h1')->text());
+    }
+
+    public function testEditAction(): void
+    {
+        $securityControllerTest = new SecurityControllerTest();
+        $client = $securityControllerTest->testLoginAsAdmin();
+
+        $crawler = $client->request('GET', '/users/2/edit');
+        static::assertSame(200, $client->getResponse()->getStatusCode());
+
+        // Test if edit page field exists
+        static::assertSame(1, $crawler->filter('input[name="user[username]"]')->count());
+        static::assertSame(1, $crawler->filter('input[name="user[password][first]"]')->count());
+        static::assertSame(1, $crawler->filter('input[name="user[password][second]"]')->count());
+        static::assertSame(1, $crawler->filter('input[name="user[email]"]')->count());
+        static::assertSame(2, $crawler->filter('input[name="user[roles][]"]')->count());
+
+        $form = $crawler->selectButton('Modifier')->form();
+        $form['user[username]'] = 'user';
+        $form['user[password][first]'] = 'test';
+        $form['user[password][second]'] = 'test';
+        $form['user[email]'] = 'editedUser@example.org';
+        $form['user[roles][0]']->tick();
+        $client->submit($form);
+        static::assertSame(302, $client->getResponse()->getStatusCode());
+
+        $crawler = $client->followRedirect();
+        static::assertSame(200, $client->getResponse()->getStatusCode());
+    }
+
+    public function testCreateAction(): void
+    {
+        $securityControllerTest = new SecurityControllerTest();
+        $client = $securityControllerTest->testLoginAsAdmin();
+
+        $crawler = $client->request('GET', '/users/create');
+        static::assertSame(200, $client->getResponse()->getStatusCode());
+
+        // Test if creation page field exists
+        static::assertSame(1, $crawler->filter('input[name="user[username]"]')->count());
+        static::assertSame(1, $crawler->filter('input[name="user[password][first]"]')->count());
+        static::assertSame(1, $crawler->filter('input[name="user[password][second]"]')->count());
+        static::assertSame(1, $crawler->filter('input[name="user[email]"]')->count());
+        static::assertSame(2, $crawler->filter('input[name="user[roles][]"]')->count());
+
+        $form = $crawler->selectButton('Ajouter')->form();
+        $form['user[username]'] = 'newuser';
+        $form['user[password][first]'] = 'test';
+        $form['user[password][second]'] = 'test';
+        $form['user[email]'] = 'newUser@example.org';
+        $form['user[roles][0]']->tick();
+        $client->submit($form);
+        static::assertSame(302, $client->getResponse()->getStatusCode());
+
+        $crawler = $client->followRedirect();
+        static::assertSame(200, $client->getResponse()->getStatusCode());
+    }
+}
